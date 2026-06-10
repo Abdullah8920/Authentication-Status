370eb091a5596f11aff5cf5b611df15847473ee5
Abdullah8920 <abdullahansari7463@gmail.com>
2026-06-10 15:43:16 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-=======
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
