b7e3dd9098c8e3480450536649bd22a1056b1f3e
Abdullah Mushtaq <abdullahansari7463@gmail.com>
2026-06-10 14:56:03 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router'
+import Portfolio from './my/portfolio'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Portfolio />
+    </BrowserRouter>
+  )
+}
+
+export default App
